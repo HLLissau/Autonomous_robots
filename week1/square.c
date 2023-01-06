d8a75304df2120af0fbc1397a1552a2fdc170736
HLLissau <68223372+HLLissau@users.noreply.github.com>
2023-01-06 13:17:47 +0100

    Update square.c

--- week1/square.c
+++ week1/square.c
@@ -338,7 +338,7 @@ int main(int argc, char **argv) {
             case ms_follow_line:
                 //7.3
                 if (mission.time==0) odo.theta_ls=0;
-                if (mission.time%10==0) odo.theta_ls++;
+                if (mission.time%10==0) odo.theta_ls=odo.theta_ls+0.04;
                 if(follow_line(dist,0.3,mission.time)) mission.state=ms_end;
 
                 break;
