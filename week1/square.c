d43ce27ac5877ece3ed9cbe81ba89cda7a6435c4
HLLissau <68223372+HLLissau@users.noreply.github.com>
2023-01-09 11:23:02 +0100

    Update square.c

--- week1/square.c
+++ week1/square.c
@@ -626,7 +626,7 @@ int follow_line_left(double dist, double speed, int time) {
         mot.cmd = mot_follow_line;
         mot.speedcmd = speed;
         mot.dist = dist;
-        mot.follow_line_diff=4.542;
+        mot.follow_line_diff=4.45;
         return 0;
     } else {
         return mot.finished;
@@ -637,7 +637,7 @@ int follow_line_right(double dist, double speed, int time) {
         mot.cmd = mot_follow_line;
         mot.speedcmd = speed;
         mot.dist = dist;
-        mot.follow_line_diff=4.568;
+        mot.follow_line_diff=4.65;
         return 0;
     } else {
         return mot.finished;
