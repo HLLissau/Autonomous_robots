1c5780cbf682485b9faef5557ce0cbef4339b1e7
hllissau <hllissau@gmail.com>
2023-01-11 12:47:12 +0100

    added square.h

---
+++ src/square.h
@@ -0,0 +1,170 @@
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <math.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include "componentserver.h"
+#include "rhd.h"
+#include "xmlio.h"
+struct xml_in *xmldata;
+struct xml_in *xmllaser;
+struct
+{
+    double x, y, z, omega, phi, kappa, code, id, crc;
+} gmk;
+double visionpar[10];
+double laserpar[10];
+volatile int running = 1;
+int robot_port;
+
+void serverconnect(componentservertype *s);
+void xml_proc(struct xml_in *x);
+void xml_proca(struct xml_in *x);
+
+componentservertype lmssrv, camsrv;
+
+symTableElement *getinputref(const char *sym_name, symTableElement *tab) {
+    int i;
+    for (i = 0; i < getSymbolTableSize('r'); i++)
+        if (strcmp(tab[i].name, sym_name) == 0)
+            return &tab[i];
+    return 0;
+}
+
+symTableElement *getoutputref(const char *sym_name, symTableElement *tab) {
+    int i;
+    for (i = 0; i < getSymbolTableSize('w'); i++)
+        if (strcmp(tab[i].name, sym_name) == 0)
+            return &tab[i];
+    return 0;
+}
+/*****************************************
+ * odometry
+ */
+#define WHEEL_DIAMETER 0.06522 /* m */
+#define WHEEL_SEPARATION 0.26  /* m */
+#define DELTA_M (M_PI * WHEEL_DIAMETER / 2000)
+#define DEFAULT_ROBOTPORT 24902
+
+#define FREQUENCY 100
+#define ACCELLERATION 0.5
+#define TICK_ACCELLERATION ACCELLERATION / FREQUENCY
+#define K 0.18
+#define K2 3  //
+#define LINE_SENSOR_DATA_LENGTH 8
+#define LINESENSORDIST 0.0185
+
+double speed = 0.2;
+double line_array[LINE_SENSOR_DATA_LENGTH];  // variable som line sensor data skal lægges ind i 7.1
+double jarray[LINE_SENSOR_DATA_LENGTH];      // normalisered værdi af line sensor.
+typedef struct
+{                             // input signals
+    int left_enc, right_enc;  // encoderticks
+    // parameters
+    double w;       // wheel separation
+    double cr, cl;  // meters per encodertick
+                    // output signals
+    double right_pos, left_pos;
+    double x, y, theta;                               // tilført 3.2
+    double Delta_theta, Delta_U, delta_Ur, delta_Ul;  // tilført 3.2
+    int len;                                          // Tilført 5.using zoneobst with square
+    double delta_v, theta_ref, theta_ls;              // Tilført 7.1
+    int location_line_sensor;                         // 7.2
+    // internal variables
+    int left_enc_old, right_enc_old;
+    float COM;  // 7.3
+} odotype;
+
+void reset_odo(odotype *p);
+void update_odo(odotype *p);
+void writeToFile();
+void sm_saveArray();
+void calibrateLinesensor();
+void read_linesensor();
+float center_of_mass(double *intensity_array);
+
+/********************************************
+ * Motion control
+ */
+
+typedef struct
+{  // input
+    int cmd;
+    int curcmd;
+    double speedcmd;
+    double dist;
+    double angle;
+    double left_pos, right_pos;
+    // parameters
+    double w;
+    // output
+    double motorspeed_l, motorspeed_r;
+    int finished;
+    // internal variables
+    double startpos;
+    // follow line offset to follow left or right
+    double follow_line_diff;
+} motiontype;
+
+enum {
+    mot_stop = 1,
+    mot_move,
+    mot_rev,
+    mot_follow_line,
+    mot_turn
+};
+
+void update_motcon(motiontype *p);
+
+int fwd(double dist, double speed, int time);
+int rev(double dist, double speed, int time);
+int turn(double angle, double speed, int time);
+int follow_line(double dist, double speed, int time, int follow);
+int follow_line_left(double dist, double speed, int time, int follow);
+int follow_line_right(double dist, double speed, int time, int follow);
+double find_laser_min();
+int crossdetection(double *array);
+
+void segfaulthandler(int sig) {
+    //    perror(NULL);
+    printf("Seg-error\n");
+    exit(1);
+}
+
+void brokenpipehandler(int sig) {
+    printf("mrc: broken pipe \n");
+    // savelog("log");
+    exit(1);
+}
+
+void ctrlchandler(int sig) {
+    printf("mrc: ctrl-c \n");
+    running = 0;
+}
+
+typedef struct
+{
+    int state, oldstate;
+    int time;
+} smtype;
+
+
+
+void sm_update(smtype *p);
+
+// SMR input/output data
+
+symTableElement *inputtable, *outputtable;
+symTableElement *lenc, *renc, *linesensor, *irsensor, *speedl, *speedr,
+    *resetmotorr, *resetmotorl;
+
+odotype odo;
+smtype mission;
+motiontype mot;
